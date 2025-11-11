7ce743212335d7096d193340f8609ac40690c45e
TerexitariusStomp <TerexitariusStomp@users.noreply.github.com>
2025-11-10 22:41:53 -0300

    Update logo references to /logo.png

--- Amazon-Clone-Project-codebase/src/components/sections/hero-section.tsx
+++ Amazon-Clone-Project-codebase/src/components/sections/hero-section.tsx
@@ -8,7 +8,7 @@ const HeroSection = () => {
       
       <div className="relative z-10 flex flex-col items-center justify-center p-6 text-center">
         <Image
-          src="https://slelguoygbfzlpylpxfs.supabase.co/storage/v1/object/public/test-clones/f9dbff3c-828d-4087-a500-734899d703f7-amazone-coop30-on77bsx-gamma-site/assets/images/ae65513d-3168-4f6d-b890-656785ecda48-1.png"
+          src="/logo.png"
           alt={t("hero.alt")}
           width={400}
           height={350}
